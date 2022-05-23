5a2e0d0dd8945188fe8d93c54fcf52bbe6f47ee9
Nathan Atkin <nathangatkin@dal.ca>
2022-05-23 20:14:51 -0300

    Added basic spring boot project

---
+++ group7-trello/src/main/java/com/group7/group7trello/Group7TrelloApplication.java
@@ -0,0 +1,13 @@
+package com.group7.group7trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group7TrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group7TrelloApplication.class, args);
+    }
+
+}
---
+++ group7-trello/src/test/java/com/group7/group7trello/Group7TrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.group7trello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group7TrelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
