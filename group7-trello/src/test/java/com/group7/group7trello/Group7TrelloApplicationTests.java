cd92dab54dbf482cf664770658673efd3705df7d
Nathan Atkin <nathangatkin@dal.ca>
2022-05-31 21:33:34 -0300

    Added first test to check db connection

--- group7-trello/src/test/java/com/group7/group7trello/Group7TrelloApplicationTests.java
+++ group7-trello/src/test/java/com/group7/group7trello/Group7TrelloApplicationTests.java
@@ -1,13 +1,41 @@
 package com.group7.group7trello;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 
 @SpringBootTest
 class Group7TrelloApplicationTests {
 
+    @Autowired
+    DataSource csci3130_group07;
+
     @Test
     void contextLoads() {
+        // Add tests for controllers and services here when they are made?
     }
 
+    @Test
+    void dbConecction() {
+        try (Connection c = csci3130_group07.getConnection()) {
+            assert true;
+        } catch (SQLException e) {
+            assert false;
+        }
+    }
+
+    void register() {
+
+    }
+
+    void login() {
+
+    }
 }
