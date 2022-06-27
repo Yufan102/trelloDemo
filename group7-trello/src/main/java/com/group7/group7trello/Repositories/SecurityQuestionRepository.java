de11492be890f423c181848813edfd3366413bc7
Yufan Zhou <yf377227@dal.ca>
2022-06-27 13:20:22 -0300

    Adding Security question Repo interface

---
+++ group7-trello/src/main/java/com/group7/group7trello/Repositories/SecurityQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.group7.group7trello.Repositories;
+
+import com.group7.group7trello.Models.SecurityQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SecurityQuestionRepository extends JpaRepository<SecurityQuestion, Long> {
+}
