b6cded60780bdc885f264c1b914d128becddf41a
Yufan <yufan.zhou@reeldata.ai>
2022-06-29 16:26:44 -0300

    Adding UserServiceImpTest

--- group7-trello/src/test/java/com/group7/group7trello/Services/UserServiceImplTest.java
+++ group7-trello/src/test/java/com/group7/group7trello/Services/UserServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.group7.group7trello.Services;
 
+import com.group7.group7trello.Models.SecurityQuestion;
 import com.group7.group7trello.Models.User;
+import com.group7.group7trello.Repositories.SecurityQuestionRepository;
 import com.group7.group7trello.Repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,16 +10,28 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.lang.management.OperatingSystemMXBean;
+import java.security.Provider;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class UserServiceImplTest {
 
     @Mock
     @Autowired
     private UserRepository userRepository;
+
+    @Mock
+    @Autowired
+    private SecurityQuestionRepository securityQuestionRepository;
 
     @InjectMocks
     private UserService userService = new UserServiceImpl();
@@ -42,17 +56,28 @@ class UserServiceImplTest {
 
     @Test
     void resetUserPassword() {
+        User user = new User();
+        User newUser = new User();
+        Mockito.when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
     }
 
     @Test
     void getUserByID() {
+        User user = new User();
+        Mockito.when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        assertNotNull(userService.getUserByID(anyLong()));
+
     }
 
     @Test
     void deleteUser() {
+        User user = new User();
+        userService.deleteUser(user);
     }
 
     @Test
     void addSecurityQuestion() {
+        SecurityQuestion securityQuestion = new SecurityQuestion();
+        Mockito.when(securityQuestionRepository.save(any())).thenReturn(securityQuestion);
     }
 }
