e57300265ff7dbd9a145e672d4f58d2732bedc06
Yufan <yufan.zhou@reeldata.ai>
2022-06-30 15:36:12 -0300

    Return JSON instead of string

--- group7-trello/src/main/java/com/group7/group7trello/Controllers/UserController.java
+++ group7-trello/src/main/java/com/group7/group7trello/Controllers/UserController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -61,20 +58,21 @@ public class UserController {
     }
 
     @PostMapping(value = "/login", consumes = "application/json", produces = "application/json")
-    public String login(@RequestBody Map<String, String> loginInfo){
+    public Map<String,String> login(@RequestBody Map<String, String> loginInfo){
         String email = loginInfo.get("email");
         String password = loginInfo.get("password");
-
-
+        HashMap<String,String>map = new HashMap<>();
 
         User u = userService.getUserByEmail(email);
         if(u == null || !u.getPassword().equals(password)) {
-            return "{'UUID':''}";
+            map.put("UUID","");
+            return map;
         }
 
         Authorization testIfUserIsAlreadyValid = authorizationService.getLatestByUser(u);
         if(testIfUserIsAlreadyValid != null && authorizationService.isValid(testIfUserIsAlreadyValid.getUuid())) {
-            return String.format("{'UUID':'%s'}", testIfUserIsAlreadyValid.getUuid());
+            map.put("UUID",testIfUserIsAlreadyValid.getUuid());
+            return map;
         }
 
         String UUID = tokenService.generateToken();
@@ -86,6 +84,7 @@ public class UserController {
         Authorization a = new Authorization(u, UUID, c.getTime());
         authorizationService.addAuthorization(a);
 
-        return String.format("{'UUID':'%s'}", UUID);
+        map.put("UUID",UUID);
+        return map;
     }
 }
