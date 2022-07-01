00b6d3a3e074565fa612f4d85f2a34f90c8a54f9
Rawan Morshed <rw788956@dal.ca>
2022-07-01 15:41:39 -0300

    Login handles invalid credentials

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -7,6 +7,9 @@ function LoginPage() {
     const url = process.env.REACT_APP_URL;
 
     function loginUserHandler(user) {
+
+
+
         fetch(url + '/user/login', {
             headers: {
                 'Accept': 'application/json',
@@ -15,15 +18,19 @@ function LoginPage() {
             method: 'POST',
             body: JSON.stringify(user)
         })
-        .then((res) => {
+        .then(async (res) => {
             //perform any other operations you want here first then redirect to homepage
             //stuff like checking if it's a valid email or if the password matches the password criteria etc
-            if (res == null) {
-                console.log(res)
+            const data = await res.json();
+            console.log('data', data.UUID);
+            if (data.UUID.length === 0) {
+                alert("Invalid email or password.")
             } else {
                 history.replace('/home')
             }
            
+        }).catch(err => {
+            console.log('err', err);
         }); //same URL as registeration, URL for database 
     }
 
