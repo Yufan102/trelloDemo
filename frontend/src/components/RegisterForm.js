c26a84eab542f5065cfff268d48d4fa0353bdbe4
Rawan Morshed <rw788956@dal.ca>
2022-07-08 17:58:55 -0300

    Forget password and validate password done

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,8 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import {Grid, TextField, makeStyles, FormControl, FormLabel, Select} from '@material-ui/core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faEyeSlash, faTimes, faCheck, faBorderAll } from '@fortawesome/free-solid-svg-icons'
-import styles from './RegisterForm.module.css'
+import React, { useState, useEffect } from 'react';
+import { Grid, TextField, makeStyles, FormControl, FormLabel, Select } from '@material-ui/core';
+import validator from 'validator';
 
 const fetchData = async (url) => {
     return fetch(url)
@@ -10,7 +8,7 @@ const fetchData = async (url) => {
         .catch(error => console.log('error', error))
 }
 
-const useStyle = makeStyles(theme =>({
+const useStyle = makeStyles(theme => ({
     root: {
         '& .MuiFormControl-root': {
             width: '90%',
@@ -18,106 +16,88 @@ const useStyle = makeStyles(theme =>({
             backgroundColor: 'FloralWhite',
             borderRadius: 6,
             boxShadow: ' 1px 2px 9px DarkSlateBlue',
-        },        
+        },
     }
 }))
 
-function RegisterForm(props){
-    const [fisrtNameValue, setFirstName] = useState('') 
-    const [lastNameValue, setLastName] = useState('') 
-    const [emailValue, setEmail] = useState('') 
-    const [passwordValue, setPassword] = useState('') 
-    const [answerValue, setAnswer] = useState('') 
+function RegisterForm(props) {
+    const [fisrtNameValue, setFirstName] = useState('')
+    const [lastNameValue, setLastName] = useState('')
+    const [emailValue, setEmail] = useState('')
+    const [passwordValue, setPassword] = useState('')
+    const [answerValue, setAnswer] = useState('')
     const [question_id, setQuestionID] = useState('')
     const [show, setShow] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('')
 
-    function submitHandler(event){
+    const validate = (passwordValue) => {
+
+        if (validator.isStrongPassword(passwordValue, {
+            minLength: 8, minLowercase: 1,
+            minUppercase: 1, minNumbers: 1, minSymbols: 1
+        })) {
+            setErrorMessage('Strong password')
+            setPassword(passwordValue)
+        } else {
+            setErrorMessage('Password is not strong enough, please see requirements.')
+        }
+    }
+
+    function submitHandler(event) {
         event.preventDefault();
         //Read the values
-        
+
         const first_name = fisrtNameValue;
         const last_name = lastNameValue;
         const email = emailValue;
         const password = passwordValue;
         const answer = answerValue;
-        const user = {first_name, last_name, email, password, answer, question_id};
-        
+        const user = { first_name, last_name, email, password, answer, question_id };
+
         //Send the values to server
-        props.registerUser(user);
+        if (errorMessage == "Strong password") {
+            props.registerUser(user);
+        } else {
+            alert("Weak password. Please re-enter.")
+        }
+
     }
 
     const classes = useStyle();
     const [questions, setQuestions] = useState([]); //--> keep data in component state
 
-    const showHide = () => {
-        setShow(!show);
-    };
-
-    const valid = (item, v_icon, inv_icon) => {
-        let text = document.querySelector(`#${item}`);
-        text.style.opacity = 1;
-
-        let valid_icon = document.querySelector(`#${item} .${v_icon}`);
-        valid_icon.style.opacity = 1;
-
-        let invalid_icon = document.querySelector(`#${item} .${inv_icon}`);
-        invalid_icon.style.opacity = 0;
-    };
-
-    const invalid = (item, v_icon, inv_icon) => {
-        let text = document.querySelector(`#${item}`);
-        text.style.opacity = .5;
-
-        let valid_icon = document.querySelector(`#${item} .${v_icon}`);
-        valid_icon.style.opacity = 0;
-
-        let invalid_icon = document.querySelector(`#${item} .${inv_icon}`);
-        invalid_icon.style.opacity = 1;
-    }
-
-    const handlePasswordChange = e => {
-        setPassword(e.target.value)
-
-        console.log(passwordValue.test(/A-Z/))
-        if ( passwordValue.test(/A-Z/) == null) {
-            valid("capital", "fa-check", "fa-times");
-        } else {
-            invalid("capital", "fa-check", "fa-times")
-        }
-    }
-
-    useEffect(()=> {
+    useEffect(() => {
         fetchData('http://localhost:8080/api/question/getAll').then(response => setQuestions(response))
     }, []);
 
     return (
         <form className={classes.root} onSubmit={submitHandler} autoComplete="off">
-           <Grid container>
+            <Grid container>
                 <Grid item xs={6}>
                     <TextField
                         variant="outlined"
                         label="First Name"
                         name="first_name"
                         value={fisrtNameValue}
-                        onChange={(e) => setFirstName(e.target.value)} 
+                        onChange={(e) => setFirstName(e.target.value)}
                         required
-                    />  
+                    />
                     <TextField
                         variant="outlined"
                         label="Last Name"
                         name="last_name"
                         value={lastNameValue}
-                        onChange={(e) => setLastName(e.target.value)} 
+                        onChange={(e) => setLastName(e.target.value)}
                         required
-                    />   
+                    />
                     <TextField
                         variant="outlined"
                         label="Email"
                         name="email"
                         value={emailValue}
-                        onChange={(e) => setEmail(e.target.value)} 
+                        onChange={(e) => setEmail(e.target.value)}
                         required
-                    />               
+                    />
                 </Grid>
                 <Grid item xs={6}>
                     <FormControl>
@@ -134,59 +114,32 @@ function RegisterForm(props){
                         label="Security Answer"
                         name="answer"
                         value={answerValue}
-                        onChange={(e) => setAnswer(e.target.value)} 
+                        onChange={(e) => setAnswer(e.target.value)}
                         required
-                    /> 
-                    <div className={styles.validate}>
-                        <TextField
-                            type={show ? "text" : "password"}
-                            variant="outlined"
-                            label="Password"
-                            name="password"
-                            value={passwordValue}
-                            onChange={(e) => setPassword(e.target.value)} 
-                            //onChange={handlePasswordChange}
-                            required 
-                            minLength = '8'
-                        /> 
-                        
-                        {show ? (
-                            <FontAwesomeIcon 
-                            icon={faEye} 
-                            className={styles.show_hide} 
-                            onClick={showHide} 
-                            />
-                        ) : (
-                            <FontAwesomeIcon icon={faEyeSlash} 
-                            className={styles.show_hide} 
-                            onClick={showHide} 
-                            />
-                        )}
-
-                        <p className={styles.para} id="capital">
-                                <FontAwesomeIcon className="fa-times icon" icon={faTimes} />
-                                <FontAwesomeIcon className="fa-check icon" icon={faCheck} />
-                                <span>Capital Letters</span>
+                    />
+                    <pre>
+                        <input type={show ? "text" : "password"} required placeholder='Password'
+                            onChange={(e) => validate(e.target.value)}></input> <br />
+                        {errorMessage === '' ? null :
+                            <span style={{
+                                fontWeight: 'bold',
+                                color: 'FireBrick',
+                                fontFamily: 'sans-serif',
+                                fontSize: 'medium'
+                            }}>{errorMessage}</span>}
+                        <h4>Password requirements:</h4>
+                        <p>
+                            <ul>
+                                <li>Minimum length of 8 characters</li>
+                                <li>At least 1 uppercase character</li>
+                                <li>At least 1 lowercase character</li>
+                                <li>At least 1 number</li>
+                                <li>At least 1 special character</li>
+                            </ul>
                         </p>
-                        <p className={styles.para} id="char">
-                                <FontAwesomeIcon className={styles.faTimes} icon={faTimes} />
-                                <FontAwesomeIcon className={styles.faCheck} icon={faCheck} />
-                                <span>Special Characters</span>
-                        </p>
-                        <p className={styles.para} id="num">
-                                <FontAwesomeIcon className={styles.faTimes} icon={faTimes} />
-                                <FontAwesomeIcon className={styles.faCheck} icon={faCheck} />
-                                <span>Use Numbers</span>
-                        </p>
-                        <p className={styles.para} id="more8">
-                                <FontAwesomeIcon className={styles.faTimes} icon={faTimes} />
-                                <FontAwesomeIcon className={styles.faCheck} icon={faCheck} />
-                                <span>8+ Characters</span>
-                        </p>
-
-                    </div>
+                    </pre>
                 </Grid>
-           </Grid>
+            </Grid>
             <button>Submit</button>
         </form>
     );
--- frontend/src/pages/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword.js
@@ -1,109 +1,113 @@
 import ForgotForm from "../components/forgotForm";
-import React, {useState} from 'react';
-import {Grid, TextField, makeStyles, FormControl, FormLabel, Select} from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid, TextField, makeStyles, FormControl, FormLabel, Select } from '@material-ui/core';
 
-export function EmailVerification(question, email) {
+export function EmailVerification({ question, email }) {
     const [answerValue, setAnswer] = useState('');
     const [newpasswordValue, setNewPassword] = useState('');
     const url = process.env.REACT_APP_URL;
 
-    if(question == ""){
-        alert("No account exists with this email. Please enter a valid email.")
-    } else {
-        function submitHandlerReset(event) {
-            event.preventDefault();
-            const answer = answerValue;
-            const newPassword = newpasswordValue;
-            const userInfo = {email, answer, newPassword};
-    
-            fetch(url + '/user/forget/reset', {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: 'POST',
-                body: JSON.stringify(userInfo)
-            })
-            .then(async (res) => {
-                const newPass = await res.json();
-                console.log(newPass);
-    
-                if (newPass.new_password == ""){
-                    alert("Answer is not correct. Try again.")
-                } else if (newPass.new_password == "same") {
-                    alert("New password cannot be the same as the old password. Try again.")
-                } else {
-                    alert("Password changed successfully.")
-                }
-            })
-            .catch(err => {
-                console.log('err', err);
-            }); 
-        }
+    function submitHandlerReset(event) {
+        event.preventDefault();
+        const answer = answerValue;
+        const newPassword = newpasswordValue;
+        const userInfo = { email, ans: answer, new_password: newPassword };
+        console.log(userInfo)
 
-        return(
-            <form onSubmit={submitHandlerReset}>
-                <Grid container>
-                    <Grid item xs = {6}>
-                        <p>{question}</p>
-                        <TextField
-                            variant="outlined"
-                            label="Answer"
-                            name="answer"
-                            value={answerValue}
-                            onChange={(e) => setAnswer(e.target.value)} 
-                            required
-                        />  
-                        <TextField
-                            variant="outlined"
-                            label="New Password"
-                            name="new_password"
-                            value={newpasswordValue}
-                            onChange={(e) => setNewPassword(e.target.value)} 
-                            required
-                        />  
-                    </Grid>
-                </Grid>
-                <button>Submit</button>
-            </form>
-        )
+        fetch(url + '/user/forget/reset', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(userInfo)
+        }).then(async (res) => {
+            const newPass = await res.json();
+            console.log(newPass);
+
+            if (newPass.new_password == "") {
+                alert("Answer is not correct. Try again.")
+            } else if (newPass.new_password == "same") {
+                alert("New password cannot be the same as the old password. Try again.")
+            } else {
+                alert("Password changed successfully.")
+            }
+        }).catch(err => {
+            console.log('err', err);
+        });
+
+
     }
-    
-                // <form onSubmit={submitHandler}>
-            //     <p>{question}</p>
-            //     <input type="text" required placeholder="Answer" ref={answerRef}/>
-            //     <input type="text" required placeholder="New Password" ref={newPasswordRef}/>
-            //     <button>Submit</button>
-            // </form>
 
+    return (
+        <form onSubmit={submitHandlerReset}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <p>{question}</p>
+                    <TextField
+                        variant="outlined"
+                        label="Answer"
+                        name="answer"
+                        value={answerValue}
+                        onChange={(e) => setAnswer(e.target.value)}
+                        required
+                    />
+                    <TextField
+                        variant="outlined"
+                        label="New Password"
+                        name="new_password"
+                        value={newpasswordValue}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        required
+                    />
+                </Grid>
+            </Grid>
+            <button>Submit</button>
+        </form>
+    )
 }
+
 
 function ForgotPassword() {
     const url = process.env.REACT_APP_URL;
     const [question, setQuestion] = useState('');
-    
+    const [showEmailPlaceholder, setShowEmailPlaceholder] = useState(false);
+    const [email, setEmail] = useState('');
+
     function resetPasswordHandler(email) {
+        console.log(email)
+        setEmail(email);
         fetch(url + '/user/forget?email=' + email, {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             method: 'GET',
+
         })
-        .then((response) => response.json())
-        .then((responseData => setQuestion(responseData.question)))
-        .catch(err => {
-            console.log('err', err);
-        }); 
+            .then((response) => response.json())
+            .then(res => {
+                if (res.question == "") {
+                    setShowEmailPlaceholder(false);
+                    alert("Email id doesnt exist");
+                } else {
+                    setQuestion(res.question);
+                    setShowEmailPlaceholder(true);
+                }
+            })
+            // (responseData => setQuestion(responseData.question)) 
+            .catch(err => {
+                console.log('err', err);
+            });
     }
 
     console.log(question);
 
-    return(
+    return (
         <div>
             <h1>Enter Email to Reset Password</h1>
             <ForgotForm resetPassword={resetPasswordHandler} />
-            {EmailVerification(question)}
+            {showEmailPlaceholder ? <EmailVerification question={question} email={email}></EmailVerification> : <></>}
         </div>
     );
 
